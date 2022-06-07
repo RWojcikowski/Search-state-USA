ccd0fa661e8b1bb6fe95ef0711b5651d8bd4ede9
Radosław Wójcikowski <71291273+RWojcikowski@users.noreply.github.com>
2022-06-08 00:39:28 +0200

    added logic for element selection

--- src/components/App.js
+++ src/components/App.js
@@ -1,102 +1,104 @@
-import React from 'react';
-import { useState, useEffect } from 'react' ;
+import React, { useState, useEffect } from 'react';
 import Fuse from 'fuse.js';
-import states  from '../states.json';
+import states from '../states.json';
 import classnames from 'classnames';
-
+import StatePage from './StatePage';
 
 const options = {
-    keys: [
-        'state',
-        'code',
-    ]
-  };
-  
-  const fuse = new Fuse(states, options);
-
-const App = () => {
-const[query, setQuery] = useState('');
-const[resultList, setResultList] = useState([]);
-const[selected, setSelected] = useState(0);
-console.log(selected);
-
-const onSelect = (event) => {
-const {key} = event;
-
-if (key === 'ArrowUp'){
-    const newSelected = selected -1;
-
-if(newSelected < 0){
-return;
-}
-
-    setSelected(newSelected)
-}
-
-if (key === 'ArrowDown'){
-    const newSelected = selected +1;
-
-    if(newSelected > resultList.length - 1){
-        return;
-    }
-
-    setSelected(newSelected)
-}
-
-
+  keys: ['state', 'code'],
 };
 
-console.log({ resultList });
+const fuse = new Fuse(states, options);
 
-useEffect(() => {
-    setResultList(fuse
-        .search(query)
-        .slice(0, 8)
-        );
-        
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [resultsList, setResultsList] = useState([]);
+  const [selected, setSelected] = useState(0);
+  const [picked, setPicked] = useState(null);
 
- }, [query]);
- 
+  const onKeyUp = (event) => {
+    const { key } = event;
 
+    if (key === 'ArrowUp') {
+      const newSelected = selected - 1;
 
-    return(
-        <div className="dropdown is-active">
-            <div className="dropdown-trigger">
-                <input
-                value={query}
-                onChange={({target})=> setQuery(target.value)} 
-                className="input"
-                type="text"
-                placeholder="SEARCH STATE ..."
-                onKeyUp={onSelect}
-                />
+      if (newSelected < 0) {
+        return;
+      }
+
+      setSelected(newSelected);
+    }
+
+    if (key === 'ArrowDown') {
+      const newSelected = selected + 1;
+
+      if (newSelected > resultsList.length - 1) {
+        return;
+      }
+
+      setSelected(newSelected);
+    }
+
+    if (key === 'Enter') {
+      setPicked(selected);
+    }
+  };
+
+  useEffect(() => {
+    setResultsList(fuse.search(query).slice(0, 6));
+  }, [query]);
+
+  if (picked !== null) {
+    const currentState = resultsList[picked];
+
+    return (
+      <div className="App">
+<h1></h1>
+
+        <StatePage
+          state={currentState.item}
+          onBack={() => setPicked(null)}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className="App">
+      <div className="dropdown is-active">
+        <div className="dropdown-trigger">
+          <input
+            value={query}
+            onChange={({ target }) => setQuery(target.value)}
+            className="input"
+            type="text"
+            placeholder="Search..."
+            onKeyUp={onKeyUp}
+          />
         </div>
-    <div 
-        className="dropdown-menu" 
-        id="dropdown-menu" 
-        role="menu">
-        <div className="dropdown-content">
-            {resultList.map((
-            { item: {state, code} }, 
-            index,
-            )=> {
-                return(
-                    <a 
-                    key = {code}
-                    className={classnames("dropdown-item", {
-                        'is-active': selected === index
+        {resultsList.length > 0 && (
+          <div className="dropdown-menu">
+            <div className="dropdown-content">
+              {resultsList.map(({ item: { state, code } }, index) => {
+                return (
+                  <a
+                    key={code}
+                    className={classnames('dropdown-item', {
+                      'is-active': selected === index,
                     })}
                     onMouseEnter={() => setSelected(index)}
-
-                    >
-                 {state}
+                    onClick={() => setPicked(selected)}
+                  >
+                    {state}
                   </a>
                 );
-            })} 
-        </div>
+              })}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-</div>
-)
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import '../node_modules/bulma/css/bulma.min.css';
 import App from './components/App';
+import './style.css'
 render(
   <App />,
   document.getElementById('root'),
