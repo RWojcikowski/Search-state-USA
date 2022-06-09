d656e577049e72dd23ec75235e3682743ea1bc55
RWojcikowski <radoslaw.chicqen@gmail.com>
2022-06-09 17:32:33 +0200

    refractor code + Extract StateSearch

--- src/components/App.js
+++ src/components/App.js
@@ -1,104 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import Fuse from 'fuse.js';
-import states from '../states.json';
-import classnames from 'classnames';
-import StatePage from './StatePage';
-
-const options = {
-  keys: ['state', 'code'],
-};
-
-const fuse = new Fuse(states, options);
+import React, { useState, useEffect } from "react";
+import Fuse from "fuse.js";
+import states from "../states.json";
+import classnames from "classnames";
+import StatePage from "./StatePage";
+import StateSearch from "./StateSearch";
 
 const App = () => {
-  const [query, setQuery] = useState('');
-  const [resultsList, setResultsList] = useState([]);
-  const [selected, setSelected] = useState(0);
-  const [picked, setPicked] = useState(null);
-
-  const onKeyUp = (event) => {
-    const { key } = event;
-
-    if (key === 'ArrowUp') {
-      const newSelected = selected - 1;
-
-      if (newSelected < 0) {
-        return;
-      }
-
-      setSelected(newSelected);
-    }
-
-    if (key === 'ArrowDown') {
-      const newSelected = selected + 1;
-
-      if (newSelected > resultsList.length - 1) {
-        return;
-      }
-
-      setSelected(newSelected);
-    }
-
-    if (key === 'Enter') {
-      setPicked(selected);
-    }
-  };
-
-  useEffect(() => {
-    setResultsList(fuse.search(query).slice(0, 6));
-  }, [query]);
-
-  if (picked !== null) {
-    const currentState = resultsList[picked];
-
-    return (
-      <div className="App">
-        <StatePage state={currentState.item} onBack={() => setPicked(null)} />
-      </div>
-    );
-  }
-
   return (
     <div className="App">
-<section className="hero">
-  <div className="hero-body">
-    <p className="title">
-     Search states USA
-    </p>
-  </div>
-</section>
-
-      <div className="dropdown is-active">
-        <div className="dropdown-trigger">
-          <input
-            value={query}
-            onChange={({ target }) => setQuery(target.value)}
-            className="input"
-            type="text"
-            placeholder="Search..."
-            onKeyUp={onKeyUp}
-          />
+      <section className="hero">
+        <div className="hero-body">
+          <p className="title">Search states USA</p>
         </div>
-        {resultsList.length > 0 && (
-          <div className="dropdown-menu">
-            <div className="dropdown-content">
-              {resultsList.map(({ item: { state, code } }, index) => {
-                return (
-                  <a
-                    key={code}
-                    className={classnames('dropdown-item', {
-                      'is-active': selected === index,
-                    })}
-                    onMouseEnter={() => setSelected(index)}
-                    onClick={() => setPicked(selected)}
-                  >
-                    {state}
-                  </a>
-                );
-              })}
-            </div>
-          </div>
-        )}
+      </section>
+
+      <div className="field">
+        <label className="label ">Search by:</label>
+        <div class="select is-info">
+          <select>
+            <option>Name</option>
+            <option>Code state </option>
+            <option>Capital city </option>
+          </select>
+        </div>
+      </div>
+
+      <StateSearch />
+
+      <div className="field">
+        <label className="label ">Show Data:</label>
+        <div class="select is-info">
+          <select>
+            <option>Table</option>
+            <option>Symbols</option>
+            <option>Map</option>
+          </select>
+        </div>
       </div>
     </div>
   );
